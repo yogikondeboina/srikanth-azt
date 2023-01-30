6eec59d023bf7151fd4e6952a734dd618a106f37
yogikondeboina <108824752+yogikondeboina@users.noreply.github.com>
2023-01-30 14:45:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,14 +12,14 @@ terraform {
     storage_account_name = "demorgtfstore"
     container_name       = "terraform"
     key                  = "terraform.tfstate"
-    #use_oidc             = true
+    use_oidc             = true
     access_key           = "ZSdK6JS6S8xnn1V7LgL31lZmHB42GLAmwIBwe3r3yNqNkP58wfnjigIH77VuAolhI4wb7qQpUNqO+AStA1f2kA=="
   }
 }
 
 provider "azurerm" {
   features {}
-  #use_oidc = true
+  use_oidc = true
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
